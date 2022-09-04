7b60a2f0f858365b5cee1a38689983e40ca09040
DESKTOP-RP4BFD4\AKI <ahsi.aki@gmail.com>
2022-09-04 23:35:21 +0800

    0904 環境建置

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
+// 通用的斷點數值
+// 平板斷點：992px ->用tail的 lg 1024 px
+// 手機斷點：767px ->用tail的 md 768 px
+
+
+// Breakpoint prefix	Minimum width	CSS
+// sm	640px	@media (min-width: 640px) { ... }
+// md	768px	@media (min-width: 768px) { ... }
+// lg	1024px	@media (min-width: 1024px) { ... }
